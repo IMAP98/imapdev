d100d110b6e712d80b45712be191786e998a6fc5
Ignacio Moises AP <ignacio.moises.arteaga.pelcast@gmail.com>
2026-06-22 22:09:41 -0600

    style: :bug: Adjust the “base” parameter in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/NOMBRE_REPOSITORIO/',
+    base: '/imapdev/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
